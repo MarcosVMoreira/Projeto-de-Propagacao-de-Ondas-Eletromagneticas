07560c26e79ca865635225b3bbf906fbfa811cd3
Marcos Moreira <contato.marcosvmoreira@gmail.com>
2018-11-30 20:16:46 -0200

    Estrutura inicial

---
+++ ProjetoPropagacaoEngSoftware/src/Model/ModelPrincipal.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Marcos
+ */
+public class ModelPrincipal {
+
+    private float alturaTx;
+    private float alturaRx;
+    private float potTransmissor;
+    private float atenuacaoConector;
+    private float ganhoAntenaTx;
+    private float ganhoAntenaRx;
+    private float distanciaRadioEnlace;
+    private float freqIda;
+    private float freqVolta;
+
+    /**
+     * @return the alturaTx
+     */
+    public float getAlturaTx() {
+        return alturaTx;
+    }
+
+    /**
+     * @param alturaTx the alturaTx to set
+     */
+    public void setAlturaTx(float alturaTx) {
+        this.alturaTx = alturaTx;
+    }
+
+    /**
+     * @return the alturaRx
+     */
+    public float getAlturaRx() {
+        return alturaRx;
+    }
+
+    /**
+     * @param alturaRx the alturaRx to set
+     */
+    public void setAlturaRx(float alturaRx) {
+        this.alturaRx = alturaRx;
+    }
+
+    /**
+     * @return the potTransmissor
+     */
+    public float getPotTransmissor() {
+        return potTransmissor;
+    }
+
+    /**
+     * @param potTransmissor the potTransmissor to set
+     */
+    public void setPotTransmissor(float potTransmissor) {
+        this.potTransmissor = potTransmissor;
+    }
+
+    /**
+     * @return the atenuacaoConector
+     */
+    public float getAtenuacaoConector() {
+        return atenuacaoConector;
+    }
+
+    /**
+     * @param atenuacaoConector the atenuacaoConector to set
+     */
+    public void setAtenuacaoConector(float atenuacaoConector) {
+        this.atenuacaoConector = atenuacaoConector;
+    }
+
+    /**
+     * @return the ganhoAntenaTx
+     */
+    public float getGanhoAntenaTx() {
+        return ganhoAntenaTx;
+    }
+
+    /**
+     * @param ganhoAntenaTx the ganhoAntenaTx to set
+     */
+    public void setGanhoAntenaTx(float ganhoAntenaTx) {
+        this.ganhoAntenaTx = ganhoAntenaTx;
+    }
+
+    /**
+     * @return the ganhoAntenaRx
+     */
+    public float getGanhoAntenaRx() {
+        return ganhoAntenaRx;
+    }
+
+    /**
+     * @param ganhoAntenaRx the ganhoAntenaRx to set
+     */
+    public void setGanhoAntenaRx(float ganhoAntenaRx) {
+        this.ganhoAntenaRx = ganhoAntenaRx;
+    }
+
+    /**
+     * @return the distanciaRadioEnlace
+     */
+    public float getDistanciaRadioEnlace() {
+        return distanciaRadioEnlace;
+    }
+
+    /**
+     * @param distanciaRadioEnlace the distanciaRadioEnlace to set
+     */
+    public void setDistanciaRadioEnlace(float distanciaRadioEnlace) {
+        this.distanciaRadioEnlace = distanciaRadioEnlace;
+    }
+
+    /**
+     * @return the freqIda
+     */
+    public float getFreqIda() {
+        return freqIda;
+    }
+
+    /**
+     * @param freqIda the freqIda to set
+     */
+    public void setFreqIda(float freqIda) {
+        this.freqIda = freqIda;
+    }
+
+    /**
+     * @return the freqVolta
+     */
+    public float getFreqVolta() {
+        return freqVolta;
+    }
+
+    /**
+     * @param freqVolta the freqVolta to set
+     */
+    public void setFreqVolta(float freqVolta) {
+        this.freqVolta = freqVolta;
+    }
+
+}
